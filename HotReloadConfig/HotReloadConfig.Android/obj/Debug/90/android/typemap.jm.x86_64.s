ad190a35012af2c8bdd6eba134015c3119e47c4e
Fernando Soares <boti.fernando@gmail.com>
2020-05-04 18:48:55 -0300

    Implementação da extensão do HotReload. Permite visualização em tempo real das alterações do front-end do Xamarin Forms

---
+++ HotReloadConfig/HotReloadConfig.Android/obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 0b7ff035f7fa05f44b4e65337f6b2efe62b9f50d */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1284
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
